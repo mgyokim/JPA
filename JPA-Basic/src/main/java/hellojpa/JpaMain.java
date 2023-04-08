5a0a72e7d819b03a27622b5b7644d59cc14ba8c0
mgkim-developer <mgkim.developer@gmail.com>
2023-04-08 16:26:33 +0900

    2023.04.08_(JPA-Basic 영속성 컨텍스트2)

--- JPA-Basic/src/main/java/hellojpa/JpaMain.java
+++ JPA-Basic/src/main/java/hellojpa/JpaMain.java
@@ -18,15 +18,23 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
             // 영속
-            System.out.println("===BEFORE===");
-            em.persist(member);
-            System.out.println("===AFTER===");
+//            Member findMember1 = em.find(Member.class, 99L);    // SELECT 쿼리는 한번만 나감
+//            Member findMember2 = em.find(Member.class, 99L);    // 1차캐시에 저장해둔 영속 엔티티를 찾음
+//
+//            System.out.println("result = " + (findMember1 == findMember2)); // result = true영속성 엔티티의 동일성 보장
+
+            // 트랜잭션을 지원하는 쓰기 지연
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//
+//            em.persist(member1);
+//            em.persist(member2);
+//
+//            System.out.println("=============================");    // 쿼리 나가는 거 보기 위한 선
+            // 변경 감지를 통한 엔티티 수정 - 플러시 (엔티티와 스탭샷 비교)
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
 
             tx.commit();
         } catch (Exception e) {
