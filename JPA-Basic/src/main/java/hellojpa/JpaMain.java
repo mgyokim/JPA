69ab15ac7bb07bea8472d60f2ba86f12a78d170c
mgkim-developer <mgkim.developer@gmail.com>
2023-04-08 16:38:21 +0900

    2023.04.08_(JPA-Basic 플러시)

--- JPA-Basic/src/main/java/hellojpa/JpaMain.java
+++ JPA-Basic/src/main/java/hellojpa/JpaMain.java
@@ -18,24 +18,14 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 영속
-//            Member findMember1 = em.find(Member.class, 99L);    // SELECT 쿼리는 한번만 나감
-//            Member findMember2 = em.find(Member.class, 99L);    // 1차캐시에 저장해둔 영속 엔티티를 찾음
-//
-//            System.out.println("result = " + (findMember1 == findMember2)); // result = true영속성 엔티티의 동일성 보장
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
-            // 트랜잭션을 지원하는 쓰기 지연
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-//
-//            System.out.println("=============================");    // 쿼리 나가는 거 보기 위한 선
-            // 변경 감지를 통한 엔티티 수정 - 플러시 (엔티티와 스탭샷 비교)
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
+            em.flush(); // 플러시를 직접 호출하면, 변경감지를 통해(쓰기 지연 SQL 저장소에 있는 SQL 쿼리들) 그 즉시 DB 반영 (1차 캐시(영속성 컨텍스트)는 그대로)
 
+            // JPQL 실행시 플러시 자동 호출된다.
+
+            System.out.println("=================================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
