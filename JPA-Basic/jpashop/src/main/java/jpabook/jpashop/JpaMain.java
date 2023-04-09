440be8c96ecfb42514df7c7e3f668f3007470186
mgkim-developer <mgkim.developer@gmail.com>
2023-04-09 16:15:24 +0900

    2023.04.09_(JPA-Basic 실전 예제 1 - 요구사항 분석과 기본 매핑)

---
+++ JPA-Basic/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        // EntityManagerFactory DB당 1개(하나만 생성에서 애플리케이션 전체에서 공유)
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // EntityManager는 고객의 요청당 하나씩 썻다가 버렸다가 함. (쓰레드간에 공유X, 사용하고 버려야 한다.)
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Order order = em.find(Order.class, 1L);
+            Member findMember = order.getMember();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
