9cb7edf67be32047aefc8a0cb2c842ee25a659d2
mgkim-developer <mgkim.developer@gmail.com>
2023-04-10 13:44:48 +0900

    2023.04.10_(JPA-Basic 실전 예제 3 - 다양한 연과노간계 매핑)

---
+++ JPA-Basic/jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> chid = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),    // 내가 조인해야 되는것은 얘고
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID")  // 반대쪽이 조인해야 하는 것은 얘야.
+    )
+    private List<Item> items = new ArrayList<>();
+
+}
