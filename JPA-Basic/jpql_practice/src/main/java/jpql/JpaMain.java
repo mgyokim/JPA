ee24c492065338fa46a4b0ff0e2417fff92218ce
mgkim-developer <mgkim.developer@gmail.com>
2023-04-14 12:27:16 +0900

    2023.04.14_(JPA-Basic 페이징)

--- JPA-Basic/jpql_practice/src/main/java/jpql/JpaMain.java
+++ JPA-Basic/jpql_practice/src/main/java/jpql/JpaMain.java
@@ -18,62 +18,27 @@ public class JpaMain {
         tx.begin();
 
         try {
+            for (int i = 0; i < 100; i++) {
 
-            Member member = new Member();
-            member.setUsername("member1");
-            member.setAge(10);
-            em.persist(member);
+                Member member = new Member();
+                member.setUsername("member" + i);
+                member.setAge(i);
+                em.persist(member);
+
+            }
 
             em.flush();
             em.clear();
-// ----------- 엔티티 프로젝션
-             List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
-            Member findMember = result.get(0);
-            findMember.setAge(20);
 
-            // 묵시적 조인
-            List<Team> result2 = em.createQuery("select m.team from Member m", Team.class) // 이렇게 쓰지말고
-                    .getResultList();
-            // 명시적 조인
-            // 아래처럼 실제 sql과 최대한 비슷하게 작성해야함 왜냐하면, join의 경우 성능에 영향을 줄 수 있는 요소와 튜닝할 수 있는 요소가 많기 때문에 한눈에 보여야 한다.
-            List<Team> result3 = em.createQuery("select t from Member m join m.team t", Team.class)
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(1)
+                    .setMaxResults(10)
                     .getResultList();
 
-// ----------- 임베디드 타입 프로젝션 -> 임베디드 타입만으로는 안되고, 속한 엔티티로부터 시작 ex) o.address
-            em.createQuery("select o.address from Order o", Address.class)
-                    .getResultList();
-
-// ----------- 스칼라 타입 프로젝션
-            em.createQuery("select distinct m.username, m.age from Member m")
-                    .getResultList();
-
-// 여러 값 프로젝션 조회 - 1. Query 타입으로 조회
-            List resultList = em.createQuery("select distinct m.username, m.age from Member m")
-                    .getResultList();
-
-            Object o = resultList.get(0);
-            Object[] result4 = (Object[]) o;
-            System.out.println("username = " + result4[0]);
-            System.out.println("age = " + result4[1]);
-
-
-            // 2. 제네릭에 Obejct 배열 지정
-            List<Object[]> resultList2 = em.createQuery("select distinct m.username, m.age from Member m")
-                    .getResultList();
-
-            Object[] result5 = resultList2.get(0);
-            System.out.println("username = " + result5[0]);
-            System.out.println("age = " + result5[1]);
-
-            // 3. new 명령어로 조회 -> 가장 깔끔함
-            List<MemberDTO> result6 = em.createQuery("select distinct new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
-                    .getResultList();
-
-            MemberDTO memberDTO = result6.get(0);
-            System.out.println("memberDTO.getUsername() = " + memberDTO.getUsername());
-            System.out.println("memberDTO.getAge() = " + memberDTO.getAge());
-
+            System.out.println("result.size() = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
