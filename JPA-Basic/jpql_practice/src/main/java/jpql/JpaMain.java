cf3edb53d04c07f75c0c8fe06ba9571fa8a584e3
mgkim-developer <mgkim.developer@gmail.com>
2023-04-15 18:52:43 +0900

    2023.04.15_(JPA-Basic 벌크 연산)

--- JPA-Basic/jpql_practice/src/main/java/jpql/JpaMain.java
+++ JPA-Basic/jpql_practice/src/main/java/jpql/JpaMain.java
@@ -33,30 +33,33 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
+            // FLUSH 자동 호출
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+
+//             영속성 컨텍스트에 20살로 반영 안되어 있는 문제로 인한 데이터 정합성 문제 발생을 막기위해 영속성 컨텍스트를 clear()해줌.
             em.clear();
 
-            // JPQL - Named 쿼리
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            Member findMember = em.find(Member.class, member1.getId());
 
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
+            System.out.println("findMember = " + findMember.getAge());
+
 
             tx.commit();
         } catch (Exception e) {
