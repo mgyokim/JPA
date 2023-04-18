26a42f94f1da7f3d508cf49cb0daf5d94ecd483f
mgkim-developer <mgkim.developer@gmail.com>
2023-04-18 17:40:52 +0900

    2023.04.18_(JPA application1 엔티티 클래스 개발2)

---
+++ JPA-application1/jpashop/src/main/java/jbabook/jpashop/domain/Category.java
@@ -0,0 +1,37 @@
+package jbabook.jpashop.domain;
+
+import jbabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+
+    @ManyToMany // 실무에서는 사용하면 안된다.
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+
+}
