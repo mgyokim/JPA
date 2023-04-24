a37104f554797270667300bf3dc3c3f40b2961cb
mgkim-developer <mgkim.developer@gmail.com>
2023-04-24 19:50:56 +0900

    2023.04.24_(JPA application1 주문 목록 검색, 취소)

--- JPA-application1/jpashop/src/main/java/jbabook/jpashop/service/OrderService.java
+++ JPA-application1/jpashop/src/main/java/jbabook/jpashop/service/OrderService.java
@@ -8,6 +8,7 @@ import jbabook.jpashop.domain.item.Item;
 import jbabook.jpashop.repository.ItemRepository;
 import jbabook.jpashop.repository.MemberRepository;
 import jbabook.jpashop.repository.OrderRepository;
+import jbabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -64,7 +65,7 @@ public class OrderService {
     /**
      * 검색
      */
-//    public List<Order> findOrders(OrderSearch orderSearch) {
-//        return orderRepository.findAll(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByCriteria(orderSearch);
+    }
 }
