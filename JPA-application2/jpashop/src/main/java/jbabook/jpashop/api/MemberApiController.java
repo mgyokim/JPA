1d328769037fe659449cdcb9348800a03bcfff35
mgkim-developer <mgkim.developer@gmail.com>
2023-04-25 23:52:27 +0900

    2023.04.25_(JPA application2 회원 조회 API)

--- JPA-application2/jpashop/src/main/java/jbabook/jpashop/api/MemberApiController.java
+++ JPA-application2/jpashop/src/main/java/jbabook/jpashop/api/MemberApiController.java
@@ -9,12 +9,44 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
 
     /**
      * 등록 V1 : 요청 값으로 Member 엔티티를 직접 받는다.
