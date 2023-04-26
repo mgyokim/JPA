a5ca7f5a60416285b047fe6a57bf8f66ef44cb6d
mgkim-developer <mgkim.developer@gmail.com>
2023-04-26 19:27:03 +0900

    2023.04.26_(JPA application2 간단한 주문 조회 V2:엔티티를 DTO로 변환)

--- JPA-application2/jpashop/src/main/java/jbabook/jpashop/api/OrderSimpleApiController.java
+++ JPA-application2/jpashop/src/main/java/jbabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jbabook.jpashop.api;
 
+import jbabook.jpashop.domain.Address;
 import jbabook.jpashop.domain.Order;
+import jbabook.jpashop.domain.OrderStatus;
 import jbabook.jpashop.repository.OrderRepository;
 import jbabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne) 관계 최적화
@@ -33,7 +38,36 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress();   // Lazy 강제 초기화
         }
         return all;
+    }
 
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // ORDER 2개
+        // N + 1 -> 1 + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
     }
 
 }
